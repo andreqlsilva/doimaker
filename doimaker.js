fde01c1e9db79aa87f989d4dd686bab14ee7429a
Andre Silva <andreqlsilva@gmail.com>
2025-10-30 22:25:58 -0400

    refactor: improve prop order and styles

--- doimaker.js
+++ doimaker.js
@@ -319,58 +319,6 @@ const doiJson = `{
     }
   },
   "Imovel": {
-    "indicadorNaoConstaValorBaseCalculoItbiItcmd": {
-      "type": "boolean",
-      "description": "Assinalar a caixa se o valor da base de cálculo do ITBI/ITCMD não constar do documento"
-    },
-    "indicadorNaoConstaValorOperacaoImobiliaria": {
-      "type": "boolean",
-      "description": "Assinalar a caixa se o valor da operação imobiliária não constar do documento"
-    },
-    "indicadorPagamentoDinheiro": {
-      "type": "boolean",
-      "description": "Informar se houve pagamento em dinheiro"
-    },
-    "indicadorPermutaBens": {
-      "type": "boolean",
-      "description": "Informar se houve permuta de bens na operação imobiliária"
-    },
-    "indicadorAlienacaoFiduciaria": {
-      "type": "boolean",
-      "description": "Informar se o imóvel foi objeto de alienação fiduciária na operação"
-    },
-    "formaPagamento": {
-      "info":"FormaPagamento",
-      "type": "string",
-      "description": "Selecionar a forma de pagamento dentre as opções da caixa",
-      "oneOf": [
-        {
-          "const": "5",
-          "title": "Quitado à vista"
-        },
-        {
-          "const": "10",
-          "title": "Quitado a prazo"
-        },
-        {
-          "const": "11",
-          "title": "Quitado sem informação da forma de pagamento"
-        },
-        {
-          "const": "7",
-          "title": "A prazo"
-        },
-        {
-          "const": "9",
-          "title": "Não de aplica"
-        }
-      ]
-    },
-    "descricaoOutrasOperacoesImobiliarias": {
-      "type": "string",
-      "description": "Descrever a operação imobiliária se o valor selecionado na caixa for 'Outras Operações Imobiliárias'",
-      "maxLength": 30
-    },
     "codigoIbge": {
       "type": "string",
       "description": "Informar o código IBGE do município onde se localiza o imóvel",
@@ -390,122 +338,6 @@ const doiJson = `{
           "title": "Rural"
         }
       ]
-    },
-    "codigoNacionalMatricula": {
-      "type": "string",
-      "description": "Informar o Código Nacional de Matrícula (CNM). Formato: CCCCCCLNNNNNNNDD - O CNM informado será validado através do DV informado, seguindo o algoritmo módulo 97 base 10, conforme norma ISO 7064:2023",
-      "maxLength": 16
-    },
-    "descricaoOutrasOperacoesImobiliarias": {
-      "type": "string",
-      "description": "Descrever a operação imobiliária se o valor selecionado na caixa for 'Outras Operações Imobiliárias'",
-      "maxLength": 30
-    },
-    "inscricaoMunicipal": {
-      "type": "string",
-      "description": "Código da inscrição imobiliária",
-      "maxLength": 45
-    },
-    "indicadorAreaLoteNaoConsta": {
-      "type": "boolean",
-      "description": "Indicador de que a área do imóvel não consta nos Documentos. Vide Observações"
-    },
-    "areaImovel": {
-      "type": "number",
-      "description": "Área do lote urbano em m2 ou área do imóvel rural em ha conforme matrícula. (máx. 13 inteiros e 2 casas)."
-    },
-    "indicadorAreaConstruidaNaoConsta": {
-      "type": "boolean",
-      "description": "Indicador de que a área de construção do imóvel não consta nos Documentos"
-    },
-    "areaConstruida": {
-      "type": "number",
-      "description": "Área Construída (m2). Informar de acordo com a matrícula. Até o limite de 12 inteiros e 4 casas decimais. Preenchimento em m2"
-    },
-    "bairro": {
-      "type": "string",
-      "description": "Bairro do endereço do imóvel",
-      "maxLength": 150
-    },
-    "complementoEndereco": {
-      "type": "string",
-      "description": "Complemento do endereço do imóvel",
-      "maxLength": 100
-    },
-    "complementoNumeroImovel": {
-      "type": "string",
-      "description": "Complemente do número do endereço do imóvel",
-      "maxLength": 10
-    },
-    "cep": {
-      "type": "string",
-      "description": "CEP do endereço do imóvel",
-      "maxLength": 8
-    },
-    "cib": {
-      "type": "string",
-      "description": "Informar o código do imóvel no Cadastro Imobiliário Brasileiro (CIB). Cálculo do DV quando os caracteres originais são exclusivamente numéricos:algoritimo utilizado pelo Nirf, segundo a regra do Módulo 11. Cálculo do DV quando os caracteres originais não são exclusivamente numéricos: a) para cada caractere codificado, o seu valor será multiplicado pela sequência de fatores 4,3,9,5,7,1, e 8; b) a soma dos produtos será dividida por 31",
-      "maxLength": 8
-    },
-    "codigoIncra": {
-      "type": "string",
-      "description": "Informar o código do imóvel no Sistema Nacional de Cadastro Rural (SNCR)",
-      "maxLength": 13
-    },
-    "denominacao": {
-      "type": "string",
-      "description": "Informar o nome do imóvel rural que consta no documento (título a ser registrado, matrícula/transcrição,escritura pública etc), caso exista",
-      "maxLength": 200
-    },
-    "indicadorImovelPublicoUniao": {
-      "type": "boolean",
-      "description": "Informar se o imóvel objeto da operação imobiliária é imóvel público da União"
-    },
-    "certidaoAutorizacaoTransferencia": {
-      "type": "string",
-      "description": "Informar o número da Certidão de Autorização para Transferência (CAT) emitida pela Secretaria de Patrimônio da União (SPU)",
-      "maxLength": 11
-    },
-    "localizacao": {
-      "type": "string",
-      "description": "Informar dados que possam ajudar na localização do imóvel, tais como: distrito, povoado, colônia, núcleo, rodovia/km, ramal, gleba, lote, etc. Exemplo: Partindo da Sede do Município,margem esquerda da BR 101, Km 60",
-      "maxLength": 200
-    },
-    "matricula": {
-      "type": "string",
-      "description": "Informar o número de ordem da matrícula do imóvel",
-      "maxLength": 7
-    },
-    "numeroRegistro": {
-      "type": "string",
-      "description": "Informar o número de ordem do registro do título",
-      "maxLength": 30
-    },
-    "numeroRegistroAverbacao": {
-      "type": "string",
-      "description": "Informar o número do registro/averbação",
-      "maxLength": 7
-    },
-    "mesAnoUltimaParcela": {
-      "type": "string",
-      "format": "date",
-      "description": "Informar o mês e o ano de vencimento da última parcela para pagamento a prazo"
-    },
-    "nomeLogradouro": {
-      "type": "string",
-      "description": "Logradouro do endereço do imóvel",
-      "maxLength": 150
-    },
-    "numeroImovel": {
-      "type": "string",
-      "description": "Número do endereço do imóvel",
-      "maxLength": 10
-    },
-    "registroImobiliarioPatrimonial": {
-      "type": "string",
-      "description": "Informar a identificação do imóvel no cadastro da Secretaria de Patrimônio da União (SPU), ou seja, o número do Registro Imobiliário Patrimonial (RIP)",
-      "minLength": 13,
-      "maxLength": 13
     },
     "tipoImovel": {
       "description": "Classificação de acordo com o uso finalistico da UI",
@@ -574,6 +406,102 @@ const doiJson = `{
       "type": "string",
       "description": "Tipo logradouro do endereço do imóvel",
       "maxLength": 30
+    },
+    "nomeLogradouro": {
+      "type": "string",
+      "description": "Logradouro do endereço do imóvel",
+      "maxLength": 150
+    },
+    "complementoEndereco": {
+      "type": "string",
+      "description": "Complemento do endereço do imóvel",
+      "maxLength": 100
+    },
+    "numeroImovel": {
+      "type": "string",
+      "description": "Número do endereço do imóvel",
+      "maxLength": 10
+    },
+    "complementoNumeroImovel": {
+      "type": "string",
+      "description": "Complemente do número do endereço do imóvel",
+      "maxLength": 10
+    },
+    "bairro": {
+      "type": "string",
+      "description": "Bairro do endereço do imóvel",
+      "maxLength": 150
+    },
+    "localizacao": {
+      "type": "string",
+      "description": "Informar dados que possam ajudar na localização do imóvel, tais como: distrito, povoado, colônia, núcleo, rodovia/km, ramal, gleba, lote, etc. Exemplo: Partindo da Sede do Município,margem esquerda da BR 101, Km 60",
+      "maxLength": 200
+    },
+    "cep": {
+      "type": "string",
+      "description": "CEP do endereço do imóvel",
+      "maxLength": 8
+    },
+    "indicadorAreaLoteNaoConsta": {
+      "type": "boolean",
+      "description": "Indicador de que a área do imóvel não consta nos Documentos. Vide Observações"
+    },
+    "areaImovel": {
+      "type": "number",
+      "description": "Área do lote urbano em m2 ou área do imóvel rural em ha conforme matrícula. (máx. 13 inteiros e 2 casas)."
+    },
+    "indicadorAreaConstruidaNaoConsta": {
+      "type": "boolean",
+      "description": "Indicador de que a área de construção do imóvel não consta nos Documentos"
+    },
+    "areaConstruida": {
+      "type": "number",
+      "description": "Área Construída (m2). Informar de acordo com a matrícula. Até o limite de 12 inteiros e 4 casas decimais. Preenchimento em m2"
+    },
+    "matricula": {
+      "type": "string",
+      "description": "Informar o número de ordem da matrícula do imóvel",
+      "maxLength": 7
+    },
+    "inscricaoMunicipal": {
+      "type": "string",
+      "description": "Código da inscrição imobiliária",
+      "maxLength": 45
+    },
+    "cib": {
+      "type": "string",
+      "description": "Informar o código do imóvel no Cadastro Imobiliário Brasileiro (CIB). Cálculo do DV quando os caracteres originais são exclusivamente numéricos:algoritimo utilizado pelo Nirf, segundo a regra do Módulo 11. Cálculo do DV quando os caracteres originais não são exclusivamente numéricos: a) para cada caractere codificado, o seu valor será multiplicado pela sequência de fatores 4,3,9,5,7,1, e 8; b) a soma dos produtos será dividida por 31",
+      "maxLength": 8
+    },
+    "codigoIncra": {
+      "type": "string",
+      "description": "Informar o código do imóvel no Sistema Nacional de Cadastro Rural (SNCR)",
+      "maxLength": 13
+    },
+    "denominacao": {
+      "type": "string",
+      "description": "Informar o nome do imóvel rural que consta no documento (título a ser registrado, matrícula/transcrição,escritura pública etc), caso exista",
+      "maxLength": 200
+    },
+    "codigoNacionalMatricula": {
+      "type": "string",
+      "description": "Informar o Código Nacional de Matrícula (CNM). Formato: CCCCCCLNNNNNNNDD - O CNM informado será validado através do DV informado, seguindo o algoritmo módulo 97 base 10, conforme norma ISO 7064:2023",
+      "maxLength": 16
+    },
+    "transcricao": {
+      "type": "number",
+      "format": "int32",
+      "description": "Informar o número de ordem da transcrição. Até o limite de 8 inteiros"
+    },
+    "numeroRegistro": {
+      "type": "string",
+      "description": "Informar o número de ordem do registro do título",
+      "maxLength": 30
+    },
+    "numeroRegistroAverbacao": {
+      "type": "string",
+      "description": "Informar o número do registro/averbação",
+      "maxLength": 7
     },
     "tipoOperacaoImobiliaria": {
       "description": "Selecionar o tipo de operação imobiliária dentre as opções da caixa",
@@ -714,6 +642,19 @@ const doiJson = `{
         }
       ]
     },
+    "descricaoOutrasOperacoesImobiliarias": {
+      "type": "string",
+      "description": "Descrever a operação imobiliária se o valor selecionado na caixa for 'Outras Operações Imobiliárias'",
+      "maxLength": 30
+    },
+    "indicadorPermutaBens": {
+      "type": "boolean",
+      "description": "Informar se houve permuta de bens na operação imobiliária"
+    },
+    "indicadorAlienacaoFiduciaria": {
+      "type": "boolean",
+      "description": "Informar se o imóvel foi objeto de alienação fiduciária na operação"
+    },
     "tipoParteTransacionada": {
       "description": "Selecionar se a informação da parte transacionada do  imóvel será em percentual ou área",
       "info": "TipoParteTransacionada",
@@ -729,30 +670,84 @@ const doiJson = `{
         }
       ]
     },
-    "transcricao": {
+    "valorParteTransacionada": {
       "type": "number",
-      "format": "int32",
-      "description": "Informar o número de ordem da transcrição. Até o limite de 8 inteiros"
+      "description": "Informar a quantidade de metros/hectares ou o percentual que foi objeto da operação imobiliária, conforme opção no campo tipoParteTransacionada. Até o limite de 18 inteiros e 2 casas decimais"
     },
-    "valorBaseCalculoItbiItcmd": {
-      "type": "number",
-      "description": "Informar o valor da base de cálculo do ITBI ou do ITCMD. Até o limite de 18 inteiros e 2 casas decimais"
+    "indicadorNaoConstaValorOperacaoImobiliaria": {
+      "type": "boolean",
+      "description": "Assinalar a caixa se o valor da operação imobiliária não constar do documento"
     },
     "valorOperacaoImobiliaria": {
       "type": "number",
       "description": "Informar o valor da operação imobiliária. Até o limite de 18 inteiros e 2 casas decimais"
     },
+    "indicadorNaoConstaValorBaseCalculoItbiItcmd": {
+      "type": "boolean",
+      "description": "Assinalar a caixa se o valor da base de cálculo do ITBI/ITCMD não constar do documento"
+    },
+    "valorBaseCalculoItbiItcmd": {
+      "type": "number",
+      "description": "Informar o valor da base de cálculo do ITBI ou do ITCMD. Até o limite de 18 inteiros e 2 casas decimais"
+    },
+    "formaPagamento": {
+      "info":"FormaPagamento",
+      "type": "string",
+      "description": "Selecionar a forma de pagamento dentre as opções da caixa",
+      "oneOf": [
+        {
+          "const": "5",
+          "title": "Quitado à vista"
+        },
+        {
+          "const": "10",
+          "title": "Quitado a prazo"
+        },
+        {
+          "const": "11",
+          "title": "Quitado sem informação da forma de pagamento"
+        },
+        {
+          "const": "7",
+          "title": "A prazo"
+        },
+        {
+          "const": "9",
+          "title": "Não de aplica"
+        }
+      ]
+    },
     "valorPagoAteDataAto": {
       "type": "number",
       "description": "Informar o valor pago até a data do ato. Este campo somente deve ser  incluído se a opção 'A prazo' do campo 'forma de pagamento' for escolhida. Até o limite de 18 inteiros e 2 casas decimais"
+    },
+    "mesAnoUltimaParcela": {
+      "type": "string",
+      "format": "date",
+      "description": "Informar o mês e o ano de vencimento da última parcela para pagamento a prazo"
+    },
+    "indicadorPagamentoDinheiro": {
+      "type": "boolean",
+      "description": "Informar se houve pagamento em dinheiro"
     },
     "valorPagoMoedaCorrenteDataAto": {
       "type": "number",
       "description": "Informar o valor pago em espécie até a data do ato. Este campo somente deve ser  incluído se a informação no campo “indicadorPagamentoDinheiro” for True. Até o limite de 18 inteiros e 2 casas decimais"
     },
-    "valorParteTransacionada": {
-      "type": "number",
-      "description": "Informar a quantidade de metros/hectares ou o percentual que foi objeto da operação imobiliária, conforme opção no campo tipoParteTransacionada. Até o limite de 18 inteiros e 2 casas decimais"
+    "indicadorImovelPublicoUniao": {
+      "type": "boolean",
+      "description": "Informar se o imóvel objeto da operação imobiliária é imóvel público da União"
+    },
+    "registroImobiliarioPatrimonial": {
+      "type": "string",
+      "description": "Informar a identificação do imóvel no cadastro da Secretaria de Patrimônio da União (SPU), ou seja, o número do Registro Imobiliário Patrimonial (RIP)",
+      "minLength": 13,
+      "maxLength": 13
+    },
+    "certidaoAutorizacaoTransferencia": {
+      "type": "string",
+      "description": "Informar o número da Certidão de Autorização para Transferência (CAT) emitida pela Secretaria de Patrimônio da União (SPU)",
+      "maxLength": 11
     }
   }
 }`;
